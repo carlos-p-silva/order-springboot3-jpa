824c70e2c38a8f9f2250820254f1595da6de0f3f
Carlos Pereira <carlos.programacao@gmail.com>
2024-03-15 12:27:13 -0300

    Projeto criado

---
+++ src/main/java/com/caps/orderproject/OrderprojectApplication.java
@@ -0,0 +1,13 @@
+package com.caps.orderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caps/orderproject/OrderprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caps.orderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
